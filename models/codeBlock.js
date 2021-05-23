bd95cdf3aa235ea7b494b2c73a33f7a1ae31feb3
mthanki <mehulthanki12@gmail.com>
2021-05-23 00:23:10 -0400

    update codeblock model for tags- now an awway

--- models/codeBlock.js
+++ models/codeBlock.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 const CodeBlockSchema = new Schema({
     name: { type: String, require: true },
     code: { type: String, require: true },
-    tags: { type: String, require: true },
+    tags: { type: [String], require: true },
     creator: { type: mongoose.Types.ObjectId, required: true, ref: 'User' },
 });
 
